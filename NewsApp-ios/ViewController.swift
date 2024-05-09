7dfd0f9f9d914f3d36e0e7e1cdaefd876dd17f87
İBRAHİM YAZICI <ibrhmyazici@icloud.com>
2024-05-09 15:38:27 +0300

    Initial Commit

---
+++ NewsApp-ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsApp-ios
+//
+//  Created by İBRAHİM YAZICI on 9.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
